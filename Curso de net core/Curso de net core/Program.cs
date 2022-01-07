a17c64a3b58bf59961ec8bfa5838e7d1ad32b4ad
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-07 08:05:20 +0100

    Variables de tipo string,double,float, decimal, int

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -10,8 +10,30 @@ namespace Curso_de_net_core
 
             Console.WriteLine("Hello Victor! " + year);
 
+            //Otra forma de imprimir el valor de la variable
+            int mes = 12;
+            Console.WriteLine("Hello Victor! {0} ", mes);
+
+            //Convertir tipo string en entero
+            int xEntero = Convert.ToInt16("12");
+            double decimales = 2.0;
+            string nombre = "Joel";
+            String nombres = "Alex Joel";
+            decimal decimals = 2.07M;
+            float flotante = 8.0F;
+            Console.WriteLine("El valor decimals es: {0} ",decimals);
+            Console.WriteLine("El valor decimals es: "+ $"{decimals}");
+            Console.WriteLine(nombres + " {0} {1} {2}", xEntero, decimales, nombre);
+
             //Para que no se oculta la consola
             Console.ReadLine();
+
+            //NOTA
+            /*Para cantidades que tengan muchos decimales es mejor utilizar el
+             * tipo de datos decimal porque el tipo decimal soporta 28-29 digitos
+             * de 128 bits mientras que el double solo soporta 15-16 digitos de
+             * 64 bits y el tipo float solo soporta 7 digitos de 32 bits.
+             */
         }
     }
 }
