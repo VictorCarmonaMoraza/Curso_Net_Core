32f19bf10c0048736861ac71f13b869d988bb555
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-07 19:00:29 +0100

    8-Operadores de comprobacion de tipos y relacionales

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -9,21 +9,10 @@ namespace Curso_de_net_core
             //Operadores de asignacion
             int valor1 = 9;
             int valor2 = 2;
-            int valor3 = 6;
-            int valor4 = 3;
-            int valor5 = 9;
-            int valor6 = 3;
-            //Le sumamos al valor1 el valor2
-            valor1 += valor2;
-            valor3 -= valor4;
-            Console.WriteLine("Resultado {0} ", valor1);
-            Console.WriteLine("Resultado {0} ", valor3);
-            //Console.WriteLine("Resultado " + $"{division}");
-            Console.WriteLine("");
+            var data = valor1 < valor2;
 
-            //Obtenemos el resto de una division
-            valor6 %= valor5;
-            Console.WriteLine("Resultado {0} ", valor6);
+            Console.WriteLine("Resultado {0} ", data);
+            //Console.WriteLine("Resultado " + $"{division}");
             Console.WriteLine("");
 
             //Para que no se oculta la consola
