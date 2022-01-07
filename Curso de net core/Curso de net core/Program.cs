f9def0df992f3505baec0968a5cbdfd623661f7e
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-07 19:36:13 +0100

    10-Estructura if y los operadores condicionales

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,20 +6,19 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            //Operadores de asignacion
+            //Operadores de igualdad
             int valor1 = 9;
-            int valor2 = 2;
-            var data = valor1 == valor2;
-            //Inversa del operador de igualdad !=
-            int valor3 = 5;
-            int valor4 = 6;
-            var data2 = valor3 != valor4;
+            int valor2 = 9;
+            var data = valor1 != valor2;
 
 
-            Console.WriteLine("Resultado {0} ", data);
-            Console.WriteLine("Resultado {0} ", data2);
+            if (data)
+            {
+                Console.WriteLine("Resultado {0} ", data);
+            }
+            
             //Console.WriteLine("Resultado " + $"{division}");
-            Console.WriteLine("");
+            //Console.WriteLine("");
 
             //Para que no se oculta la consola
             Console.ReadLine();
