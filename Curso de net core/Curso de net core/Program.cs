8a27d1741968e4340156fc34c26b9bfb577a67be
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-07 18:30:51 +0100

    6-Operadores aritméticos

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,49 +6,31 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            int year = 2022;
+            int valor1 = 3;
+            int valor2 = 6;
+            var suma = valor1 + valor2;
+            Console.WriteLine("la suma es: "+$"{suma}");
+            Console.WriteLine("la suma es: {0} ", suma);
+            Console.WriteLine("");
 
-            Console.WriteLine("Hello Victor! " + year);
+            var resta = valor1 - valor2;
+            Console.WriteLine("la resta es: " + $"{resta}");
+            Console.WriteLine("la resta es: {0} ", resta);
+            Console.WriteLine("");
 
-            //Otra forma de imprimir el valor de la variable
-            int mes = 12;
-            Console.WriteLine("Hello Victor! {0} ", mes);
+            var multiplicacion = valor1 * valor2;
+            Console.WriteLine("la multiplicacion es: " + $"{multiplicacion}");
+            Console.WriteLine("la multiplicacion es: {0} ", multiplicacion);
+            Console.WriteLine("");
 
-            //Convertir tipo string en entero
-            int xEntero = Convert.ToInt16("12");
-            double decimales = 2.0;
-            string nombre = "Joel";
-            String nombres = "Alex Joel";
-            
-            float flotante = 8.0F;
-            Console.WriteLine(nombres + " {0} {1} {2} ", xEntero, decimales, nombre);
-
-            bool valor = false;
-            Console.WriteLine("El valor decimals es: {0} ", valor);
-            Console.WriteLine("El valor decimals es: " + $"{valor}");
-
-            decimal decimals = 2.07M;
-            Console.WriteLine("El valor decimals es: {0} ",decimals);
-            Console.WriteLine("El valor decimals es: "+ $"{decimals}");
-
-            Decimal de = 28.6M;
-            Console.WriteLine("El valor decimals es: {0} ", de);
-            Console.WriteLine("El valor decimals es: " + $"{de}");
-            
-            //Variable para almacenar un caracter(Solo puede almacenar un caracter)
-            char lketra = 'A';
-            Console.WriteLine("letra: "+$"{lketra}");
-            Console.WriteLine("letra {0} ",lketra);
+            var division = valor1 / valor2;
+            Console.WriteLine("la division es: " + $"{division}");
+            Console.WriteLine("la division es: {0} ", division);
+            Console.WriteLine("");
 
             //Para que no se oculta la consola
             Console.ReadLine();
 
-            //NOTA
-            /*Para cantidades que tengan muchos decimales es mejor utilizar el
-             * tipo de datos decimal porque el tipo decimal soporta 28-29 digitos
-             * de 128 bits mientras que el double solo soporta 15-16 digitos de
-             * 64 bits y el tipo float solo soporta 7 digitos de 32 bits. 
-             */
         }
     }
 }
