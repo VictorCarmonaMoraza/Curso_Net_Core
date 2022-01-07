4219786521d08fedfd11845fcc7617c882aadd5d
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-07 19:25:25 +0100

    9-OPeradores de igualdad

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -9,9 +9,15 @@ namespace Curso_de_net_core
             //Operadores de asignacion
             int valor1 = 9;
             int valor2 = 2;
-            var data = valor1 < valor2;
+            var data = valor1 == valor2;
+            //Inversa del operador de igualdad !=
+            int valor3 = 5;
+            int valor4 = 6;
+            var data2 = valor3 != valor4;
+
 
             Console.WriteLine("Resultado {0} ", data);
+            Console.WriteLine("Resultado {0} ", data2);
             //Console.WriteLine("Resultado " + $"{division}");
             Console.WriteLine("");
 
