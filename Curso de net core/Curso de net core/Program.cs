e9837daaa6ef508ecf2173c018baadec943fce5d
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-14 21:37:11 +0100

    14 - Array de tipo string

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,24 +6,28 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            //Operadores de igualdad
-            int valor1 = 9;
-            int valor2 = 98;
-            double valor3 = 10;
-            double valor4 = 11;
-            string name;
-            var data = valor1 != valor2;
+            //creacion de un array de tipo string
+            string[] cadenas = new string[5];
+            cadenas[0] = "Alex";
+            cadenas[1] = "Carlos";
+            cadenas[2] = "Alejandro";
+            cadenas[3] = "Alba";
+            cadenas[4] = "Lucia";
 
-            //if (data)
-            //{
-            //    name = "Alex";
-            //}
-            //else
-            //{
-            //    name = "Joel";
-            //}
-            name = data ? "Alex" : "Joel";
-            Console.WriteLine("Resultado {0} ", name);
+            //Inicializacion de un array
+            string[] name = { "Alex","Carlos", "Alejandro", "Alba", "Lucia" };
+
+            //Imprimimos la longitud del array
+            Console.WriteLine("Resultado {0} ", cadenas.Length);
+
+            //Imprimimos la posicion 4 del array que sera la ultima a la que podemos asignar valores
+            Console.WriteLine("Resultado {0} ", cadenas[4]);
+
+            //Concatenamos posiciones de array cadenas
+            Console.WriteLine("Resultado {0} ", cadenas[0] + " "+ cadenas[3]);
+
+            //Concatenamos posiciones de array name
+            Console.WriteLine("Resultado {0} ", name[0] + " " + name[3]);
 
             //Para que no se oculta la consola
             //Console.ReadLine();
--- Curso de net core/Readme.txt
+++ Curso de net core/Readme.txt
@@ -1,17 +1,25 @@
-## 13 - El operador condicional
+## 14 - Array de tipo string
 
-Aqui vamos a sustituir la estructura condicional if else por otra mas corta
+-Los arrays siempre empiezan desde la posicion cero
 
-        var data = valor1 != valor2;
-            //if (data)
-            //{
-            //    name = "Alex";
-            //}
-            //else
-            //{
-            //    name = "Joel";
-            //}
-            //Si el valor de data es true, la variable name tomara el valor de "Alex" mientras
-            //en caso de que sea false tomara el valor de "Joel"
-            name = data ? "Alex" : "Joel";
-            Console.WriteLine("Resultado {0} ", name);
+//creacion de un array de tipo string de longitud 5.Sino ponemos las posiciones lanza error
+string[] cadenas = new string[5];
+
+//Imprimimos la longitud del array
+Console.WriteLine("Resultado {0} ", cadenas.Length);
+
+//Asignamos en la poscion cero de array el valor de "Alex"
+cadenas[0] = "Alex";
+
+//Si intentamos meter el la posicion 5 de attay el valor "Alex" saltara una excepcion porque no podemos almacenar
+//en esa poscion porque al empezar en cero llegaria hasta 4
+cadenas[5] = "Alex";
+
+//Imprimimos la posicion 4 del array que sera la ultima a la que podemos asignar valores
+Console.WriteLine("Resultado {0} ", cadenas[4]);
+
+//Inicializacion de un array
+string[] name = { "Alex","Carlos", "Alejandro", "Alba", "Lucia" };
+
+ //Concatenamos posiciones de array name
+ Console.WriteLine("Resultado {0} ", name[0] + " " + name[3]);
