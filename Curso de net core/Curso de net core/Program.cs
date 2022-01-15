f3ff00430acfaf912a70923bbaeb4f1a426946ca
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-15 08:59:29 +0100

    17 - Bucle ciclo For

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,14 +6,15 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            //Estamos creando un array multidimensional de 3 filas y 2 columnas
-            double[,] bidimensional = new double[3, 2] { { 6, 3.0 }, { 7, 5.1 }, { 8, 4.7 } };
-            //esrtamos creando un array tridimensioanl con 2 filas, 2 columnas y tres datos en cada columna
-            double[,,] tridmensional = new double[2, 2, 3] { { { 3.0, 6.9, 8.9 }, { 6.8, 4.5, 7.6 } }, { { 7.0, 5.9, 1.9 }, { 9.8, 15.5, 78.6 } } };
-
-            //Imrprimimos la poscion que queremos
-            Console.WriteLine("Resultado {0}", bidimensional[1, 1]);
-            Console.WriteLine("Resultado {0}", tridmensional[1, 1, 1]);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Resultado {0}", i); 
+            }
+            Console.WriteLine("-------------------------");
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("Resultado {0}", i);
+            }
 
             //Para que no se oculta la consola
             //Console.ReadLine();
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,12 +1,21 @@
-﻿## 16 - Arreglos Multidimensionales
-
- //Estamos creando un array multidimensional de 3 filas y 2 columnas
-            double[,] bidimensional = new double[3, 2] { { 6, 3.0 }, { 7, 5.1 }, { 8, 4.7 } };
-
-//Estamos creando un array tridimensioanl con 2 filas, 2 columnas y tres datos en cada columna
-            double[,,] tridmensional = new double[2, 2, 3] { { { 3.0, 6.9, 8.9 }, { 6.8, 4.5, 7.6 } }, { { 7.0, 5.9, 1.9 }, { 9.8, 15.5, 78.6 } } };
+﻿## 17 - Bucle ciclo For
 
 
-//Imrprimimos la poscion que queremos
-            Console.WriteLine("Resultado {0}", bidimensional[1, 1]);
-            Console.WriteLine("Resultado {0}", tridmensional[1, 1, 1]);
+Estructura de un ciclo For
+
+ for (int i = 0; i < length; i++)
+            {
+
+            }
+
+//Imprimos del 0 hasta el 9 mediante el ciclo For
+for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Resultado {0}", i); 
+            }
+
+//Imprimimos del 10 hasta el 1 mediante el ciclo For
+for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("Resultado {0}", i);
+            }
