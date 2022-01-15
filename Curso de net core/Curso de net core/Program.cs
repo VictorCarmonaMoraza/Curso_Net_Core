522628fc71d09ad7c01258ad4566904eaa6887fd
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-15 10:33:16 +0100

    20 - Instruccion de seleccion switch

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,46 +6,21 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            //Altura del triangulo
-            int pisos = 0;
-            //Creamos una array de un posicion
-            int[] arreglo = new int[1];
+            //declaramos variable
+            var data = 5;
 
-            //Mostramos un mensaje y capturarlo el dato ingresado desde la consola
-            Console.WriteLine("Ingrese el numero de pisos");
-
-            /*Obtener datos por consola y lo transformamos en enetero ya que todo lo que
-             se captura por consola es como un string*/
-            pisos = Convert.ToInt16(Console.ReadLine());
-
-            for (int i = 0; i <= pisos; i++)
+            switch (data)
             {
-                /*Colocamos un arreglo y colocamos la variable del ciclo for
-                 que sera el tamaño que tendra el arreglo cada vez que el ciclo for
-                se ejecute*/
-                int[] pascal = new int[i];
-                for (int j = pisos; j < i; j--)
-                {
-                    Console.WriteLine("");
-                }
-                //Ciclo for que genera las sumas de las cifras
-                for (int k = 0; k < i; k++)
-                {
-                    //Condicion que evalua la variable del ciclo for
-                    if(k==0 || k == (i - 1))
-                    {
-                        pascal[k] = 1;
-                    }
-                    else
-                    {
-                        /*Sumamos los numeros que estan en cada posicion
-                         del arreglo para formar el triangulo*/
-                        pascal[k] = arreglo[k] + arreglo[k - 1];
-                    }
-                    Console.Write("[" + pascal[k] + "]");
-                }
-                arreglo = pascal;
-                Console.WriteLine(" ");
+                case 5:
+                    Console.WriteLine("Este es el numero que viene en la data y es {0}", data);
+                    break;
+                case 4:
+                    Console.WriteLine("Este es el numero que viene en la data y es {0}", data);
+                    break;
+
+                default:
+                    Console.WriteLine("Este es el numero que viene en la data y es el numero {0}", data);
+                    break;
             }
 
 
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,3 +1,18 @@
-﻿## 19 - Triangulo de Pascal
+﻿## 20 - Instruccion de seleccion switch
 
-Para entenderlo bien hay que debugear cada vuelta
+ //declaramos variable
+            var data =5;
+//Comprobamos si existe algun caso donde se cumple que data se corresponde con alguno de los casos
+ switch (data)
+            {
+                case 5:
+                    Console.WriteLine("Este es el numero que viene en la data y es 5");
+                    break;
+                case 4:
+                    Console.WriteLine("Este es el numero que viene en la data y es 4");
+                    break;
+
+                default:
+                    Console.WriteLine("No es ningun numero");
+                    break;
+            }
