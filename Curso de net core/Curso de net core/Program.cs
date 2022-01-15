e59dc2dff87dd536ba3cf232fc399641e5262bc1
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-15 10:21:18 +0100

    19 - Triangulo de Pascal

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,18 +6,50 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            //Creamos una array de tipo string
-            string[] name = { "Alejandor", "Victor", "Carlos" };
+            //Altura del triangulo
+            int pisos = 0;
+            //Creamos una array de un posicion
+            int[] arreglo = new int[1];
 
-            for (int i = 0; i < name.Length; i++)
+            //Mostramos un mensaje y capturarlo el dato ingresado desde la consola
+            Console.WriteLine("Ingrese el numero de pisos");
+
+            /*Obtener datos por consola y lo transformamos en enetero ya que todo lo que
+             se captura por consola es como un string*/
+            pisos = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i <= pisos; i++)
             {
-                Console.WriteLine("Resultado {0}", name[i]);
+                /*Colocamos un arreglo y colocamos la variable del ciclo for
+                 que sera el tamaño que tendra el arreglo cada vez que el ciclo for
+                se ejecute*/
+                int[] pascal = new int[i];
+                for (int j = pisos; j < i; j--)
+                {
+                    Console.WriteLine("");
+                }
+                //Ciclo for que genera las sumas de las cifras
+                for (int k = 0; k < i; k++)
+                {
+                    //Condicion que evalua la variable del ciclo for
+                    if(k==0 || k == (i - 1))
+                    {
+                        pascal[k] = 1;
+                    }
+                    else
+                    {
+                        /*Sumamos los numeros que estan en cada posicion
+                         del arreglo para formar el triangulo*/
+                        pascal[k] = arreglo[k] + arreglo[k - 1];
+                    }
+                    Console.Write("[" + pascal[k] + "]");
+                }
+                arreglo = pascal;
+                Console.WriteLine(" ");
             }
-            Console.WriteLine("--------------------");
-            foreach (var item in name)
-            {
-                Console.WriteLine("Resultado {0}", item);
-            }
+
+
+
 
             //Para que no se oculta la consola
             //Console.ReadLine();
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,20 +1,3 @@
-﻿## 18 La estructura bucle ciclo Foreach
+﻿## 19 - Triangulo de Pascal
 
-//Creamos una array de tipo string
-            string[] name = { "Alejandor", "Victor", "Carlos" };
-
-//Recorremos el array anterior con un ciclo for y un ciclo foreach
-
--CICLO FOR
-for (int i = 0; i < name.Length; i++)
-            {
-                Console.WriteLine("Resultado {0}", name[i]);
-            }
-
-            Console.WriteLine("--------------------");
-
--CICLO FOREACH
-foreach (var item in name)
-            {
-                Console.WriteLine("Resultado {0}", item);
-            }
+Para entenderlo bien hay que debugear cada vuelta
