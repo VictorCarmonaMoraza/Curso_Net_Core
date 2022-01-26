766545cfc4d8958c492e610d75ef61c39e254617
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-26 21:13:11 +0100

    25 - Inmutabilidad de los objetos String

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -6,52 +6,15 @@ namespace Curso_de_net_core
     {
         static void Main(string[] args)
         {
-            //creamos una variable de tipo bool
-            var valor = true;
-            var count = 1;
-            while (valor)
-            {
-                if (count == 6)
-                {
-                    valor = false;
-                }
-                count++;
-                Console.WriteLine("Ejeccion de while {0}", count);
-            }
-            //---------------------------
+            string name1 = "Ale";
+            string name2 = "Victor";
+            string name3 = name1;
 
-            valor = true;
-            count = 1;
-            //Por lo menos una vez entra una vez
-            do
-            {
-                if (count == 6)
-                {
-                    valor = false;
-                }
-                count++;
-                Console.WriteLine("Ejecucion de do while {0}", count);
+            name1 += name2;
+            Console.WriteLine("Name: {0}", name1);
+            Console.WriteLine("Name: {0}", name3);
 
-            } while (valor);
-
-            valor = true;
-            count = 1;
-            //Por lo menos una vez entra una vez
-            do
-            {
-                if (count == 6)
-                {
-                    //Podemos parar la ejecucion con la palabra break
-                    //Esta palabra para toda la ejecucion y ya no se ejecuta ninguna linea mas hacia abajo
-                    break;
-                }
-                count++;
-                Console.WriteLine("Ejecucion de do while con break {0}", count);
-
-            } while (valor);
-
-            //Para que no se oculta la consola
-            //Console.ReadLine();
+            Console.ReadLine();
             Console.ReadKey();
         }
     }
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,30 +1,4 @@
-﻿## 24-La estructura do While
+﻿## 25 - Inmutabilidad de los objetos String
 
--Estructura do while
 
-            do
-            {
-                if (count == 6)
-                {
-                    valor = false;
-                }
-                count++;
-                Console.WriteLine("Ejecucion de do while {0}", count);
-
-            } while (valor);
-
--Parada del do while con un break
-
-            do
-            {
-                if (count == 6)
-                {
-                    //Podemos parar la ejecucion con la palabra break
-                    //Esta palabra para toda la ejecucion y ya no se ejecuta ninguna linea mas hacia abajo
-                    break;
-                }
-                count++;
-                Console.WriteLine("Ejecucion de do while con break {0}", count);
-
-            } while (valor);
 
