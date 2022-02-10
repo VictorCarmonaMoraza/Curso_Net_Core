c1518eb9b5ccdb3fd3fe75b3d13d9ee6c90463c0
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-02-10 07:44:30 +0100

    48 - Argumentos con nombres

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -9,20 +9,28 @@ namespace Curso_de_net_core
             var data = new Program();
             //Llamamos al metodo con dos parametros, aunque sean tres pero el ultimo es opcional
             //por lo tanto no tenemos error, pero por defecto le estamos asignando el valor 69
-            data.metodo("C#", "Victor");
-            data.metodo("C#", "Victor",115);
+            data.metodo("C#",cantidad:32);
+            data.metodo2(curso:"C#",cantidad:32, nombre:"Victor Carmona");
+            data.metodo3(null, null);
+            
+            
         }
 
-        private void metodo(String curso, string nombre, int cantidad = 69)
+        private void metodo(String curso, int cantidad, string nombre="Victor")
+        {
+            //Aqui devolvera 69 porque se asigan el que tiene por defecto
+            Console.WriteLine($"{curso} {cantidad}");
+        }
+
+        private void metodo2(String curso, int cantidad, string nombre)
         {
             //Aqui devolvera 69 porque se asigan el que tiene por defecto
             Console.WriteLine(cantidad);
         }
 
-        private void metodo2(String curso, string nombre, int cantidad = 69)
+        private void metodo3(String curso, int? cantidad, string nombre="Jorge")
         {
-            //Aqui devolvera 115 porque se lo hemos puesto en los argumentos de 
-            //llamada al metodo
+            //Aqui devolvera 69 porque se asigan el que tiene por defecto
             Console.WriteLine(cantidad);
         }
     }
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,3 +1,11 @@
-﻿## 47 - Argumentos opcionales
+﻿## 48 - Argumentos con nombres
 
-Los parametros opcionales van detras de los parametros necesarios
+//Con el signo d einterrogacion le podemos pasar valores nulos a cantidad, en los string no hace falta porque
+aceptan valores nulo
+
+data.metodo3(null, null);
+
+private void metodo3(String curso, int? cantidad, string nombre="Jorge")
+        {
+            Console.WriteLine(cantidad);
+        }
