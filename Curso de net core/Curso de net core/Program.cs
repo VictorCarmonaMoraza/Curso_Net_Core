f6ff63d814930691a353ae7d4a969c262ee5f0d3
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-02-10 08:02:10 +0100

    49 - Palabras claves ReadOnly, Const

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -4,34 +4,23 @@ namespace Curso_de_net_core
 {
     class Program
     {
+        //Es solo de lectura pero no le podremos asignar valor
+        readonly int valor = 23;
+        //Forzosamente debemos inicializar las constantes
+        const int VALOR2 = 98;
+
+        //Solo podemos modificar su valor en los constructores
+        public Program()
+        {
+            valor = 10;
+        }
+
         static void Main(string[] args)
         {
             var data = new Program();
-            //Llamamos al metodo con dos parametros, aunque sean tres pero el ultimo es opcional
-            //por lo tanto no tenemos error, pero por defecto le estamos asignando el valor 69
-            data.metodo("C#",cantidad:32);
-            data.metodo2(curso:"C#",cantidad:32, nombre:"Victor Carmona");
-            data.metodo3(null, null);
-            
-            
-        }
+            Console.WriteLine($"{data.valor}");
 
-        private void metodo(String curso, int cantidad, string nombre="Victor")
-        {
-            //Aqui devolvera 69 porque se asigan el que tiene por defecto
-            Console.WriteLine($"{curso} {cantidad}");
-        }
 
-        private void metodo2(String curso, int cantidad, string nombre)
-        {
-            //Aqui devolvera 69 porque se asigan el que tiene por defecto
-            Console.WriteLine(cantidad);
-        }
-
-        private void metodo3(String curso, int? cantidad, string nombre="Jorge")
-        {
-            //Aqui devolvera 69 porque se asigan el que tiene por defecto
-            Console.WriteLine(cantidad);
         }
     }
 }
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,11 +1,1 @@
-﻿## 48 - Argumentos con nombres
-
-//Con el signo d einterrogacion le podemos pasar valores nulos a cantidad, en los string no hace falta porque
-aceptan valores nulo
-
-data.metodo3(null, null);
-
-private void metodo3(String curso, int? cantidad, string nombre="Jorge")
-        {
-            Console.WriteLine(cantidad);
-        }
+﻿## 49 - Palabras claves ReadOnly, Const
