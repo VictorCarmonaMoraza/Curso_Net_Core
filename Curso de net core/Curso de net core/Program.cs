b968ad3021f36def8f1578b371e4f74af6dd320c
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-02-11 08:15:35 +0100

    50 - Propiedades #1

--- Curso de net core/Curso de net core/Program.cs
+++ Curso de net core/Curso de net core/Program.cs
@@ -4,23 +4,11 @@ namespace Curso_de_net_core
 {
     class Program
     {
-        //Es solo de lectura pero no le podremos asignar valor
-        readonly int valor = 23;
-        //Forzosamente debemos inicializar las constantes
-        const int VALOR2 = 98;
-
-        //Solo podemos modificar su valor en los constructores
-        public Program()
-        {
-            valor = 10;
-        }
-
         static void Main(string[] args)
         {
-            var data = new Program();
-            Console.WriteLine($"{data.valor}");
-
-
+            var data = new Estudiante();
+            var nombre = data.Nombre="Ana";
+            var apellido = data.Apellido = "Benitez";
         }
     }
 }
--- Curso de net core/Curso de net core/Readme.txt
+++ Curso de net core/Curso de net core/Readme.txt
@@ -1,1 +1,4 @@
-﻿## 49 - Palabras claves ReadOnly, Const
+﻿## 50 - Propiedades #1
+
+Hemos creado una clase nueva con los getter y setter. Un setter no se puede crear de 
+manera independiente sin inicializalo
